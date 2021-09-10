f08bb93a15e4f00add550b12bca18d65d3dbe9ca
sunsky <sunsky@SkyFox.local>
2021-09-10 20:44:14 +0800

    update progress

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
-use std::io::{self, Write};
-
 fn main() {
     let image_width = 256;
     let image_hight = 256;
     println!("P3\n{} {}\n255", image_width, image_hight);
     let mut j = 256.0;
     while j >= 0.0 {
-        io::stderr().flush().unwrap();
-        eprint!("\r");
-        eprint!("Rendering now... {}%", (256.0 - j) / 2.56);
+        eprint!("\rRendering now... {}%", (256.0 - j) / 2.56);
         let mut i = 0.0;
         while i < 256.0 {
             let r = i / (image_width - 1) as f64;
             let g = j / (image_hight - 1) as f64;
-            let b = 0.25;
             let ir = (255.999 * r) as i32;
             let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
+            let ib = (255.999 * 0.25) as i32;
 
             println!("{} {} {}", ir, ig, ib);
             i += 1.0;
