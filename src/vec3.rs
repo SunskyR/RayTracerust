4e1f5c3b3323b918bc34b805c5289cfc4700aec5
ocfox <ocfox@outlook.com>
2021-10-04 19:10:19 +0800

    add vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,17 +1,93 @@
-use std::ops::Add;
-
+use std::ops::{Add, Div, Mul, Neg, Sub};
 pub struct Vec3 {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
-impl vec3 {
-    fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
+impl Vec3 {
+    // Length
+    pub fn length(&self) -> f32 {
+        self.squared_length().sqrt()
+    }
+    // Squared length
+    pub fn squared_length(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+    // Unit vector
+    pub fn unit_vector(&self) -> Vec3 {
+        self / self.length()
+    }
+    // Inner product
+    pub fn dot(&self, rhs: &Vec3) -> f32 {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+    // Cross product
+    pub fn cross(&self, rhs: &Vec3) -> Vec3 {
         Vec3 {
-            x: e1,
-            y: e2,
-            z: e3,
+            x: self.y * rhs.z - rhs.y * self.z,
+            y: self.z * rhs.x - rhs.z * self.x,
+            z: self.x * rhs.y - rhs.x * self.y,
+        }
+    }
+}
+
+impl Add for &Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Neg for &Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Sub for &Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl Mul<f32> for &Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Vec3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl Div<f32> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Vec3 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
         }
     }
 }
